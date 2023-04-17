4d4256206cdc82e19e5c71352b0b0eca0ab15b57
Admin <Admin@STEVENVR>
2023-04-17 15:04:33 -0600

    Add project files.

---
+++ DELEITE/DELEITE/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using DELEITE.Views;
+using Xamarin.Forms.Xaml;
+
+namespace DELEITE
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            
+            MainPage = new NavigationPage(new DeleiteLoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
